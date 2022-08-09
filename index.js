8282cfa4c8002bff9b0055e1830d592651ac953e
jay-io <jay.dnb@outlook.in>
2022-08-10 01:40:38 +0530

    Merely started to code

--- index.js
+++ index.js
@@ -2,5 +2,14 @@ const express = require('express');
 const cors = require('cors');
 
 require('dotenv').config();
+require('./mdb/mdb').setup();
 
 const app = express();
+
+app.use(express.json());
+app.use(cors({
+	origin: '',
+	credentials: 'true'
+}));
+
+
